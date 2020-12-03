787a8bf35734b748db2506ee45894f660110d8d8
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-03 22:31:38 +0900

    クラスとメソッド

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,29 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by Takuya Ando on 2020/12/03.
+//
+
+import Foundation
+
+class Car {
+  
+  var frontWheel = 0
+  var rearWheel = 0
+
+//  init() {
+//    frontWheel = 2
+//    rearWheel = 2
+//  }
+//
+  func drive() {
+    print("ウンテンカイシシマス")
+    print("前輪 \(frontWheel)")
+    print("後輪 \(rearWheel)")
+  }
+  
+  func move(toBack:String) {
+    print(toBack)
+  }
+}
--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var carModel = Car()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    carModel.frontWheel = 10
+    carModel.rearWheel = 10
     // Do any additional setup after loading the view.
+  
   }
 
-
+  @IBAction func doAction(_ sender: Any) {
+    carModel.drive()
+  }
+  
 }
 
