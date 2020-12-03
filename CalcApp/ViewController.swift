48c4d57e04844f692524e37972690c6a3166400e
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-03 22:07:00 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Takuya Ando on 2020/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
